9ec3354bdd2c294869c3ace37897523e0fe7147f
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-09-19 18:56:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ttt::Application.config.secret_token = '71e8536b443b6b8a24030a32472978431a56aa98b18783194c016de624be7f6447767029ba701a185a80aa6cfa1674c2e1bd90ea84915fc2b26a2c15b7892c03'
