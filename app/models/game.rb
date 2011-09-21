c2bedd2a63fe3794b1961923ef02b51237a7e035
Ethan Soutar-Rau <esoutarr@ucsd.edu>
2011-09-21 15:10:19 -0700

    game layout

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,10 @@
 class Game < ActiveRecord::Base
 
   belongs_to :subject
-  #belongs_to :condition
+  belongs_to :experimental_condition
 
   validates_inclusion_of :dimension, :in => 3..5, :message => "must be between 3 and 5"
   validates_presence_of :experimental_condition_id
 
+  delegate :name, :to => :experimental_condition, :prefix => true
 end
