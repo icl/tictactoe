6241273699d18f4d18b1ee73e887956d71c50e88
Ethan Soutar-Rau <esoutarr@ucsd.edu>
2011-09-21 16:18:45 -0700

    sproutcore fiddling

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,1 +1,23 @@
-console.warn('wee');
+
+var TTT = SC.Application.create();
+
+TTT.Move = SC.Object.extend({
+  player: 'human',
+  position: 0
+});
+
+TTT.MovesController = SC.ArrayProxy.create({
+  content: [],
+  createMove: function(position, player) {
+    var move = TTT.Move.create({ position: position, player: player });
+    this.pushObject(move);
+  }
+});
+
+TTT.BoardView = SC.View.extend({
+ content: "Main content"
+});
+
+TTT.MoveHistory = SC.View.extend({
+ content: "Secondary content"
+});
