78f5949fe4ed3a326466c5490d84e56f675170d4
Ethan Soutar-Rau <esoutarr@ucsd.edu>
2011-10-14 14:50:28 -0700

    5x5 labels

--- app/assets/javascripts/models/game.js
+++ app/assets/javascripts/models/game.js
@@ -18,7 +18,7 @@ TTT.Game = SC.Object.extend({
       label_array[position] = "" + this.get('row_labels')[row] +  this.get('column_labels')[column]  ;
     }
     //return label_array;
-     return [ '?','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15', '16'];
+     return [ '?','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'];
   }.property('dimension', 'row_labels', 'column_labels')
 });
 
