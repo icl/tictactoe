839e90a451f269c468fe18d372340dc13f26e9bd
Ethan Soutar-Rau <esoutarr@ucsd.edu>
2011-10-14 14:47:35 -0700

    re-enable 5v5

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,7 +4,7 @@ class Game < ActiveRecord::Base
   belongs_to :experimental_condition
   has_many :moves
 
-  validates_inclusion_of :dimension, :in => 3..4, :message => "must be between 3 and 4"
+  validates_inclusion_of :dimension, :in => 3..5, :message => "must be between 3 and 4"
   validates_presence_of :experimental_condition_id
   validates_presence_of :subject_id
   delegate :name, :to => :experimental_condition, :prefix => true
