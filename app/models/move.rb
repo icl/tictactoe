1040810477482693867c3d31d36a7934e0b06c6e
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-12-09 10:22:52 -0800

    fall back to inter_move_interval when there are no previous errors

--- app/models/move.rb
+++ app/models/move.rb
@@ -63,7 +63,7 @@ ttt = TTT.new(state: game.state, dimension: game.dimension)
   end
 
   def inter_success_interval
-       all_moves = game.moves
+      all_moves = game.moves
       puts all_moves.count
       previous_moves = all_moves.reject { | a_move| a_move.id >= self.id }
       puts previous_moves.count
@@ -71,7 +71,7 @@ ttt = TTT.new(state: game.state, dimension: game.dimension)
       puts errors.count
       puts errors.inspect
       isi =  move_timestamp.to_i - errors.last.prompt_timestamp.to_i if errors.first
-      return isi
+      return isi || inter_move_interval
    end
 
   def is_error?
