2e46873fda25794ec0cfd278adacaf238c1739f7
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2011-12-12 18:28:40 -0800

    summary statistics

--- app/models/game_exporter.rb
+++ app/models/game_exporter.rb
@@ -1,10 +1,19 @@
 class GameExporter
 
-  def intialize game
+  def initialize game
     @game = game
   end
 
   def summary_attributes
-    { subject_initials: @game.subject.initials }
+    { subject_initials:                     @game.subject.initials,
+      dimension:                            @game.dimension,
+      condition:                            @game.experimental_condition.name,
+      subject_game_number:                  @game.subject_game_number,
+      total_moves:                          @game.total_moves,
+      total_valid_moves:                    @game.total_valid_moves,
+      total_invalid_moves:                  @game.total_invalid_moves,
+      mean_inter_move_interval:             @game.mean_inter_move_interval,
+      mean_inter_valid_move_interval:       @game.mean_inter_valid_move_interval
+    }
   end
 end
--- spec/models/game_exporter_spec.rb
+++ spec/models/game_exporter_spec.rb
@@ -12,17 +12,39 @@ describe GameExporter do
     it "reports the subject initials" do
       @dump[:subject_initials].should_not be_nil
     end
-    it "reports the dimension"
-    it "reports the condition"
-    it "reports the game # for that subject in that condition"
-    it "reports the game # for that dimension"
-    it "reports the game # for that subject"
-    it "reports the number of moves"
-    it "reports the number of valid moves"
-    it "reports the mean inter-move-interval"
-    it "reports the number of invalid moves"
-    it "reports the mean inter valid move interval"
-    it "reports the mean inter invalid move interval"
+    it "reports the dimension" do
+      @dump[:dimension].should_not be_nil
+    end
+    it "reports the condition"do
+      @dump[:condition].should_not be_nil
+    end
+    it "reports the game # for that subject in that condition" do
+      @dump[:subject_game_number_for_condition].should_not be_nil
+    end
+    it "reports the game # for that dimension"do
+      @dump[:subject_game_number_for_dimension].should_not be_nil
+    end
+    it "reports the game # for that subject" do
+      @dump[:subject_game_number].should_not be_nil
+    end
+    it "reports the number of moves" do
+      @dump[:total_moves].should_not be_nil
+    end
+    it "reports the number of valid moves" do
+      @dump[:total_valid_moves].should_not be_nil
+    end
+    it "reports the mean inter-move-interval" do
+      @dump[:mean_inter_move_interval].should_not be_nil
+    end
+    it "reports the number of invalid moves" do
+      @dump[:total_invalid_moves].should_not be_nil
+    end
+    it "reports the mean inter valid move interval" do
+      @dump[:mean_inter_valid_move_interval].should_not be_nil
+    end
+    it "reports the mean inter invalid move interval" do
+      @dump[:mean_inter_error_interval].should_not be_nil
+    end
 
   end
 end
